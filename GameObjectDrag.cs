14d89b3969514f11ba39821280c1c452fb76b451
Arjun Khunti <39767903+ArjunKhunti@users.noreply.github.com>
2019-05-18 12:12:59 +0530

    Drag Object

---
+++ GameObjectDrag.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameObjectDrag : MonoBehaviour
+{
+    Vector3 screenPoints;
+    Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoints = Camera.main.WorldToScreenPoint(transform.position);
+
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+            Input.mousePosition.y, screenPoints.z));
+
+        Debug.Log("Hey There");
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 CursorScreenPoints = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoints.z   );
+        Vector3 cursorPoints = Camera.main.ScreenToWorldPoint(CursorScreenPoints) + offset;
+
+        transform.position = cursorPoints;
+    }
+}
