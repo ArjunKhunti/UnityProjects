c751c6a733ba12c8f0d82ea9be9e4ae803476888
Arjun Khunti <39767903+ArjunKhunti@users.noreply.github.com>
2019-05-18 07:30:04 +0530

    Create a new object with Every Click

---
+++ RunTimePrefag.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunTimePrefag : MonoBehaviour
+{
+    Ray ray;
+    RaycastHit hit;
+    public GameObject prefab;
+    // Use this for initialization
+    void Start()
+    {
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                GameObject obj = Instantiate(prefab, new Vector3(hit.point.x, hit.point.y, hit.point.z), Quaternion.identity) as GameObject;
+
+            }
+
+        }
+
+
+
+    }
+
+
+}
